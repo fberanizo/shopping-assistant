25da2c5643efc0c26914bbb23892ac313393ea74
Fabio Beranizo <fabio.beranizo@nubank.com.br>
2024-01-22 10:40:34 -0300

    Adjusts product list

--- src/features/products/productsAPI.ts
+++ src/features/products/productsAPI.ts
@@ -1,10 +1,13 @@
 
 export function fetchProducts() {
   return Promise.resolve({data: [
-    {regex: '(AGUA MIN.SERRAS DE CUNHA S GAS PET.1.5l)', text: 'Água 1.5L'},
+    {regex: '(AGUA MIN.SERRAS DE CUNHA S GAS PET.1.5l|AGUA SERRA NEGRA 1,5L HIDRATTA)', text: 'Água 1.5L'},
+    {regex: '(CREME LEITE VEG.NOT|CR LEITE VEG)', text: 'Creme de Leite'},
+    {regex: '(ALIMENTO VEG.NOT MILK|BEB VEG NOT MILK)', text: 'Leite'},
     {regex: '(LIMAO TAITI kg)', text: 'Limão Taiti'},
     {regex: '(MAMAO PAPAYA un)', text: 'Mamão Papaya'},
-    {regex: '(OVOS NATUREGG CAIP.VERM.GDE.LIV.ANT.10un)', text: 'Ovos'},
+    {regex: '(MORANGO BD)', text: 'Morango'},
+    {regex: '(OVOS NATUREGG CAIP)', text: 'Ovos'},
     {regex: '(SOBRECOXA FGO.KORIN CONG.600g)', text: 'Sobrecoxa de Frango'},
   ]});
 }
