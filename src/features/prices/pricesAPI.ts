6c7eacf2276e1c9d24707567d6d32bce1cb22c23
Fabio Beranizo <fabio.beranizo@nubank.com.br>
2024-01-22 10:31:24 -0300

    Adds data for 16/Jan/2024 - Pague Menos

--- src/features/prices/pricesAPI.ts
+++ src/features/prices/pricesAPI.ts
@@ -5,6 +5,7 @@ import j3 from '../../data/2024-01-11-SONDA.json';
 import j4 from '../../data/2024-01-13-SONDA.json';
 import j5 from '../../data/2024-01-14-PAGUE-MENOS.json';
 import j6 from '../../data/2024-01-15-SONDA.json';
+import j7 from '../../data/2024-01-16-PAGUE-MENOS.json';
 
 const data = {
   '2023-12-22': j0,
@@ -14,6 +15,7 @@ const data = {
   '2024-01-13': j4,
   '2024-01-14': j5,
   '2024-01-15': j6,
+  '2024-01-16': j7,
 };
 
 export function fetchPrices(regex: string) {
