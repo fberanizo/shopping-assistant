c1b53aa9b4abb0e37ec72b16bc971995aeeb54d4
Fabio Beranizo <fabio.beranizo@nubank.com.br>
2024-01-29 22:25:08 -0300

    fix products

--- src/features/products/productsAPI.ts
+++ src/features/products/productsAPI.ts
@@ -1,14 +1,14 @@
 
 export function fetchProducts() {
   return Promise.resolve({data: [
-    {regex: '(AGUA MIN.SERRAS DE CUNHA S GAS PET.1.5l|AGUA SERRA NEGRA 1,5L HIDRATTA)', text: 'Água 1.5L'},
+    {regex: '(AGUA)', text: 'Água 1.5L'},
     {regex: '(CREME LEITE VEG.NOT|CR LEITE VEG)', text: 'Creme de Leite'},
-    {regex: '(ALIMENTO VEG.NOT MILK|BEB VEG NOT MILK)', text: 'Leite'},
-    {regex: '(LIMAO TAITI kg)', text: 'Limão Taiti'},
-    {regex: '(MAMAO PAPAYA un)', text: 'Mamão Papaya'},
-    {regex: '(MORANGO BD)', text: 'Morango'},
-    {regex: '(OVOS NATUREGG CAIP)', text: 'Ovos'},
+    {regex: '(NOT MILK)', text: 'Leite'},
+    {regex: '(LIMAO)', text: 'Limão Taiti'},
+    {regex: '(MAMAO)', text: 'Mamão Papaya'},
+    {regex: '(MORANGO)', text: 'Morango'},
+    {regex: '(OVOS)', text: 'Ovos'},
     {regex: '(SOBRECOXA FGO.KORIN CONG.600g)', text: 'Sobrecoxa de Frango'},
-    {regex: '(MASSA TAP.DA TERRINHA PRONTA|GOMA TAP TERR)', text: 'Tapioca'},
+    {regex: '(TAP.DA TERRINHA|GOMA)', text: 'Tapioca'},
   ]});
 }
