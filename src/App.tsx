0d2429d22fee4e93dd3d8bbf6d7796002390b085
Fabio Beranizo <fabio.beranizo@nubank.com.br>
2024-05-01 11:26:17 -0300

    fix warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
-import Products from './features/products/Products';
-import Prices from './features/prices/Prices';
 import ShoppingList from './features/shopping-list/ShoppingList';
 import './App.css';
 
 function App() {
-  const [visible, setVisible] = useState(true);
   return (
     <Layout style={{height:"100vh"}}>
       <ShoppingList />
-      {/* <Products visible={visible} setVisible={setVisible} /> */}
-      {/* <Prices visible={visible} /> */}
     </Layout>
   );
 }
